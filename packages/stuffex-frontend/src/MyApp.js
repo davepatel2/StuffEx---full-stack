5d8b98cbbb1514f8430904f0b83ee8d752e01a97
Dave Patel <davepatel@hotmail.com>
2023-11-03 16:11:59 -0700

    add item functionalit v1

--- packages/stuffex-frontend/src/MyApp.js
+++ packages/stuffex-frontend/src/MyApp.js
@@ -1,24 +1,31 @@
 // src/MyApp.js
-import React from "react";
+
 import Item from "./Item";
-const items = [
-  {
+import React, {useState} from 'react';
+import Form from './Form';
+
+function MyApp() {
+  const [items, setItems] = useState([
+    {
     title: "Apple",
     image: "example.jpg",
     description: "hello",
-    
-  },
-  {
+    },
+    {
     title: "ball",
     image:"ball.jpg",
     description:"Hi im a ball",
-  },
-];
-function MyApp() {
+    },
+  ]);
+
+  function updateList(listing) {
+    setItems([...items, listing]);
+  }
   return (
     <div className="container">
       <h1>StuffEX</h1>
       <Item itemData={items} />
+      <Form handleSubmit={updateList} />
     </div>
   );
 }
