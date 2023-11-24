064f2e776df9a627dbbebf234b516894094eb495
Keith Lee <redkiinjle@gmail.com>
2023-11-24 03:18:42 -0800

    added route to items a specific user is giving away

---
+++ packages/stuffex-frontend/src/UserItems.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import Item from './Item'
+
+function UserItems({ backendRoot }) {
+  const [userItems, setUserItems] = useState([])
+  let { userId } = useParams()
+
+  useEffect(() => {
+    fetch(`${backendRoot}/users/${userId}/items`)
+      .then((res) => res.json())
+      .then((data) => setUserItems(data))
+      .catch((error) => console.error(error))
+  }, [])
+
+  return <Item itemData={userItems} showSearchBar={false} title="Your Items" />
+}
+
+export default UserItems
