0d3917b448823af1ee1c554c200b07a5850d208e
Keith Lee <redkiinjle@gmail.com>
2023-11-24 03:19:47 -0800

    formatting

--- packages/stuffex-frontend/src/components/About.js
+++ packages/stuffex-frontend/src/components/About.js
@@ -4,9 +4,9 @@ const About = () => {
       <h2>About StuffEx</h2>
       <p>
         StuffEX is a website for individuals who want to find and get rid any
-        item. It allows people to post items they don’t want anymore so others
+        item. It allows people to post items they don't want anymore so others
         can give them a new home. Unlike Craigslist, our product is strictly for
-        free items. We aren’t connecting sellers to buyers but people to people.
+        free items. We aren't connecting sellers to buyers but people to people.
       </p>
     </div>
   )
