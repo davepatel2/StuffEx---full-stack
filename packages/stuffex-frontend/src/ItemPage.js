705dd88d94157ba770711a635a6ada585eda55d2
Keith Lee <redkiinjle@gmail.com>
2023-11-24 03:17:58 -0800

    Added image navigation and ability to enlarge selected images

--- packages/stuffex-frontend/src/ItemPage.js
+++ packages/stuffex-frontend/src/ItemPage.js
@@ -1,14 +1,22 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-
 import noImage from './images/no_image.png'
+import { Link } from 'react-router-dom'
+import './ItemPage.css'
 
 function ItemPage(props) {
+  // Get the itemId from the URL parameters
   const { itemId } = useParams()
-  const [item, setItem] = useState([])
 
+  // State variables
+  const [item, setItem] = useState([]) // Store the item data
+  const [currentImageIndex, setCurrentImageIndex] = useState(0) // Index of the currently displayed image
+  const [isModalOpen, setIsModalOpen] = useState(false) // Boolean to control modal visibility
+
+  // Backend root URL
   const backendRoot = props.backendRoot
 
+  // Fetch item data from the backend when the component mounts
   useEffect(() => {
     fetch(`${backendRoot}/items/${itemId}`)
       .then((res) => res.json())
@@ -19,18 +27,84 @@ function ItemPage(props) {
       .catch((e) => console.log(e))
   }, [backendRoot, itemId])
 
-  const imageStyle = {
-    maxWidth: '200px',
-  }
-
+  // Function to get the image source, falling back to a placeholder if no images exist
   const getImageSource = (item, index) =>
     item.images && item.images.length > 0 ? item.images[index] : noImage
 
+  // Function to navigate to the previous or next image
+  const navigateImage = (direction) => {
+    const numImages = item.images.length
+    if (numImages > 1) {
+      let newImageIndex = (currentImageIndex + direction) % numImages
+      if (newImageIndex < 0) {
+        newImageIndex += numImages
+      }
+      setCurrentImageIndex(newImageIndex)
+    }
+  }
+
+  // Function to toggle the modal's visibility
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen)
+  }
+
+  // JSX for the component
   return (
     <div>
       <h1>{item.title}</h1>
-      <img src={getImageSource(item, 0)} style={imageStyle} alt={item.title} />
+      <div className="image-navigation center-content">
+        {item.images && item.images.length > 0 ? (
+          <>
+            {item.images.length > 1 && (
+              <button
+                className="navigation-button"
+                onClick={() => navigateImage(-1)}
+              >
+                Prev
+              </button>
+            )}
+            <div className="image-container" onClick={toggleModal}>
+              <img
+                src={getImageSource(item, currentImageIndex)}
+                className="image"
+                alt={item.title}
+              />
+            </div>
+            {item.images.length > 1 && (
+              <button
+                className="navigation-button"
+                onClick={() => navigateImage(1)}
+              >
+                Next
+              </button>
+            )}
+          </>
+        ) : (
+          <img src={noImage} className="image" alt="No Image" />
+        )}
+      </div>
       <p>{item.description}</p>
+      <Link to={`/`}>Back to Item List</Link>
+      {isModalOpen && (
+        <div className="modal" onClick={toggleModal}>
+          <div className="image-modal">
+            <div className="modal-content">
+              <span className="close" onClick={toggleModal}>
+                &times;
+              </span>
+              <img
+                src={getImageSource(item, currentImageIndex)}
+                style={{
+                  maxWidth: '100%',
+                  maxHeight: '80vh',
+                  backgroundColor: 'white',
+                }}
+                alt={item.title}
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
